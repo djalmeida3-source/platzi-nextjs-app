9c439299cd1e0250e13758850c52c77236c4a041
djalmeida3 <djalmeida3@gmail.com>
2024-02-27 19:33:55 -0500

    Adding Back for Frontend implementation

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
-import { getProducts } from 'app/services/shopify';
 
-export const MainProducts = async() => {
-  const products = await getProducts();
+export const MainProducts = async () => {
+  const response = await fetch('http://localhost:3000/api');
+  const { products } = await response.json();
+  
   return (
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>

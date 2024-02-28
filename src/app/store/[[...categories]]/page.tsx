3629c0414b4e02c61341f9beb7b5cb0fef077724
djalmeida3 <djalmeida3@gmail.com>
2024-02-28 10:53:36 -0500

    Adding data fetching patterns on categories

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductsWrapper } from "app/components/Store/ProductWrapper";
-import { getProducts } from "app/services/shopify";
+import { getProducts } from "app/services/shopify/products";
+import { getCollections } from "app/services/shopify/collections";
 
 
 interface CategoriesProps {
@@ -11,6 +12,7 @@ interface CategoriesProps {
 
 export default async function Category(props: CategoriesProps) {
   const products = await getProducts();
+  
   const { categories } = props.params;
   // throw new Error('Boom')
   return (
---
+++ src/services/shopify/collections.ts
@@ -0,0 +1,23 @@
+import { env } from "app/config/env";
+import { shopyfyUrls  } from "./urls";
+
+export const getCollections = async () => {
+  try {
+    const response = await fetch(shopyfyUrls.collections.all, {
+      headers: new Headers({
+        "X-Shopify-Access-Token": env.SHOPIFY_TOKEN
+      })
+    });
+    const { smart_collections } = await response.json();
+    const transformedCollections = smart_collections.map((collection: any) => {
+      return {
+        id: collection.id,
+        title: collection.title,
+        handle: collection.handle,
+      }
+    })
+    return transformedCollections;
+  } catch (error) {
+    console.log(error);
+  }
+}
